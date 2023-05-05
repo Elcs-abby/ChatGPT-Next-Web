fddbd4409898707fae902adcbfa6fe50ab15f2a8
Elcs <597132687@qq.com>
2023-05-05 23:14:30 +0800

    okk

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -3,7 +3,7 @@ import { SubmitKey } from "../store/app";
 const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
-    Unauthorized: "现在是未授权状态，请在设置页填写授权码。",
+    Unauthorized: "You exceeded your current quota, please check your plan and billing details.",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
